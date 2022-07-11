99eacf64a2ee4ee582dc826108599f3d9fb0fa75
sunil9813 <sunilpokhrel797@gmail.com>
2022-07-11 18:03:50 +0545

    Tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useState, useEffect } from "react"
+import imgLoading from "./image/loading.gif"
 
+const url = "https://course-api.com/react-tabs-project"
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [items, setitems] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchData = async () => {
+    const response = await fetch(url)
+    const newitems = await response.json()
+    setitems(newitems)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return (
+      <div className='loading'>
+        <img src={imgLoading} alt='' />
+      </div>
+    )
+  }
+  const { company, dates, duties, title } = items[value]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <main>
+        <section>
+          <div className='items'>
+            <div className='button'>
+              {items.map((item, index) => (
+                <button key={item.id} onClick={() => setValue(index)} className={`btn ${index === value && "active"}`}>
+                  {item.company}
+                </button>
+              ))}
+            </div>
+            <div className='content'>
+              <h3>{title}</h3>
+              <h4>{company}</h4>
+              <p>{dates}</p>
+              {duties.map((duty, index) => (
+                <div className='desc'>
+                  <i class='fa-solid fa-angles-right'></i>
+                  <p>{duty}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
